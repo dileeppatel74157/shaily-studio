9d8f3894f88fb52018956a7d64ed09929ff282ec
dileeppatel74157 <dileeppatel74157@gmail.com>
2026-07-29 13:16:18 +0530

    Update import path for DEFAULT_AGENTS

--- packages/core/src/client/index.ts
+++ packages/core/src/client/index.ts
@@ -1,7 +1,7 @@
 import { AgentMetadata } from "@shaily/shared";
 
 export { SYSTEM_VERSION } from "../constants/index";
-export { DEFAULT_AGENTS } from "../agents/index";
+export { DEFAULT_AGENTS } from "../agents/types/constants";
 export { logSystemEvent } from "../logger/index";
 
 export type { AgentMetadata };
